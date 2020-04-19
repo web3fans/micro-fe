583e43898c97c1ee1cb93b5e3dba59f86031cf15
CoolSummer <147201214@qq.com>
2020-04-20 00:33:10 +0800

    micro-frontend-example

---
+++ qiankun-demo/qiankun.config.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2020-04-20 00:28:41
+ * @LastEditTime: 2020-04-20 00:28:42
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /work/micro-frontends/qiankun-demo/qiankun.config.js
+ */
